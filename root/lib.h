f09fbc683269bc062c2b2495178b630f51ae69cd
Jinglu Wang <jingluwang@dhcp-131-225-173-191.dhcp.fnal.gov>
2023-09-18 12:25:23 -0500

    Nevents=1e07, ScatterDistribution compared & drawn

---
+++ root/lib.h
@@ -0,0 +1,31 @@
+#ifndef LIB_HEADER
+#define LIB_HEADER
+
+// basic
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <fstream>
+
+// root
+#include <TROOT.h>
+#include <TFile.h>
+#include "TMath.h"
+#include "TTree.h"
+#include "TH1D.h"
+#include <THStack.h>
+#include <TF1.h>
+
+// plotting
+#include <TCanvas.h>
+#include <TLegend.h>
+#include <TPad.h>
+#include <TStyle.h>
+#include <TPaveText.h>
+#include <TLatex.h>
+#include <TGaxis.h>
+
+// fitting
+#include "TMinuit.h"
+
+#endif
